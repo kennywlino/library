b704111dc90cc6f6d5fcddc2185132edd7bd703b
Kenny W. Lino <kennywlino@gmail.com>
2022-07-12 17:37:43 -0700

    Defined example Book objects and wrote displayLibrary function to add Book into DOM.

--- javascript.js
+++ javascript.js
@@ -1,24 +1,22 @@
 let myLibrary = [];
 
-function Book (title, author, numPages, read) {
+function Book (title, authorFirst, authorLast, numPages, read) {
     this.title = title;
-    this.author = author;
+    this.authorFirst = authorFirst;
+    this.authorLast = authorLast;
     this.numPages = numPages;
     this.read = read;
-    Book.prototype.getReadSent = function() {
-        let readSent = "";
-        switch(readSent) {
+    Book.prototype.getReadSent = function(read) {
+        switch(read) {
         case false:
-            readSent = 'not read yet';
-            break;
+            return 'not read yet';
         case true:
-            readSent = 'read already';
-            break;
+            return 'read already';
         }
     };
-    this.info = (`${title} by ${author}, ${numPages} pages, ${readSent}`);
+    this.readSent = this.getReadSent(read);
+    this.info = (`${this.title} by ${this.authorFirst} ${this.authorLast}, ${this.numPages} pages, ${this.readSent}`);
 }
-
 
 // PSEUDOCODE:
 // 1. construct new Book object with user input
@@ -31,7 +29,36 @@ function removeBookFromLibrary() {
 }
 
 function displayLibrary() {
+    let divBooks = document.getElementById('books');
+    for (i = 0; i < myLibrary.length; i++) {
+        bookElem = createBookHTML(myLibrary[i]);
+        divBooks.appendChild(bookElem);
+    }
+}
+
+function createBookHTML(book) {
+    let bookElem = document.createElement('div');
+    bookElem.classList.add("book");
+    let title = document.createElement('h2');
+    title.classList.add("title");
+    title.textContent = book.title;
+    let author = document.createElement('p');
+    author.textContent = book.authorFirst + ' ' + book.authorLast;
+    /* let numPages = document.createElement('p')
+    let read = document.createElement('p'); */
+    bookElem.append(title, author);
+    return bookElem;
 }
 
 function toggleReadStatus() {
 }
+
+
+
+testBook1 = new Book("The Phantom Tollbooth", "Norton", "Juster", 255, true);
+testBook2 = new Book("Holes", "Louis", "Sachar", 272, true);
+testBook3 = new Book("Bridge to Terabithia", "Katherine", "Paterson", 208, true);
+testBook4 = new Book("The Catcher in the Rye", "J.D.", "Salinger", 234, false);
+
+myLibrary.push(testBook1, testBook2, testBook3, testBook4);
+displayLibrary();
