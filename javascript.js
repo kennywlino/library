540c3aa800b4656a85d11539248f7c6207b49034
Kenny W. Lino <kennywlino@gmail.com>
2022-07-19 21:12:00 -0700

    Added IDs to Book obj and set up Book deletion functions.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 let myLibrary = [];
+let bookId = 0;
 
 function Book (title, authorFirst, authorLast, numPages, read) {
+    this.id = bookId++;
     this.title = title;
     this.authorFirst = authorFirst;
     this.authorLast = authorLast;
@@ -23,27 +25,41 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-function removeBookFromLibrary(book) {
+// needs to remove it from the array *and* DOM
+function removeBookFromLibrary(bookDiv) {
+    let bookId = bookDiv.id.charAt((bookDiv.id.length) - 1); // book-*1*
+    bookId = Number(bookId);
+    bookDiv.remove();
+    const delBookIdx = myLibrary.findIndex(book => book.id === bookId);
+    myLibrary.splice(delBookIdx, 1);
 }
 
 // iterates over myLibrary array and creates a div per book
 // appends the div to the parent 'books' div
 function displayLibrary() {
-    let divBooks = document.getElementById('books');
     for (i = 0; i < myLibrary.length; i++) {
-        bookDiv = createBookDiv(myLibrary[i]);
-        divBooks.appendChild(bookDiv);
+        displayBook(myLibrary[i]);
     }
+}
+
+function displayBook(book) {
+    let divBooks = document.getElementById('books');
+    bookDiv = createBookDiv(book);
+    divBooks.appendChild(bookDiv);
 }
 
 // Returns a single div with Book attributes as children
 function createBookDiv(book) {
     let bookDiv = createDivElement('div', "book");
+    let bookID = "book-" + book.id;
+    bookDiv.setAttribute("id", bookID);
+    let deleteButton = createDivElement('button', "delete");
+    deleteButton.textContent = "Delete";
     let titleElem = createDivElement('h2', "title", book.title);
     let authorElem = createDivElement('p', "author", (book.authorFirst + ' ' + book.authorLast));
-    let numPagesElem = createDivElement('p', "numPages", String(book.numPages));
+    let numPagesElem = createDivElement('p', "num-pages", String(book.numPages));
     let readElem = createDivElement('p', "read", String(book.read));
-    bookDiv.append(titleElem, authorElem, numPagesElem, readElem);
+    bookDiv.append(deleteButton, titleElem, authorElem, numPagesElem, readElem);
     return bookDiv;
   }
 
@@ -58,7 +74,24 @@ function createDivElement(elementType, className, text) {
     return elem;
 }
 
-function toggleReadStatus() {
+// takes in a Book obj and changes the book status
+function toggleReadStatus(book) {
+    if (book.read == true) {
+        book.read = false;
+    } else {
+        book.read = true;
+    }
+}
+
+// adds an event listener to "Delete" buttons on each book
+function setUpDeleteListener() {
+    const deleteBookButtons = document.querySelectorAll("#books .delete");
+    deleteBookButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            parentBookDiv = button.parentNode;
+            removeBookFromLibrary(parentBookDiv);
+        });
+    });
 }
 
 // adds an event listener to the "Add Book" button
@@ -82,26 +115,31 @@ function setUpAddToLibraryListener() {
         newBook = processBookForm();
         addBookToLibrary(newBook);
         // hide the form;
-        displayLibrary();
-    }
+        displayBook(newBook);
+    });
 }
 
 // takes user input from book form and returns new Book obj
 function processBookForm() {
-    const bookTitle = document.querySelector("#book_info #book_title");
-    const authorFirst = document.querySelector("#book_info #author_first");
-    const authorLast = document.querySelector("#book_info #author_last");
-    const numPages = document.querySelector("#book_info #num_pages");
-    newBook = new Book(bookTitle.value, authorFirst.value, authorLast.value, numPages.value, true);
+    const bookTitle = document.querySelector("#book-info #book-title");
+    const authorFirst = document.querySelector("#book-info #author-first");
+    const authorLast = document.querySelector("#book-info #author-last");
+    const numPages = document.querySelector("#book-info #num-pages");
+    const readStatus = document.querySelector("#book-info #read").checked;
+    newBook = new Book(bookTitle.value, authorFirst.value, authorLast.value, numPages.value, readStatus);
     return newBook;
 }
 
 testBook1 = new Book("The Phantom Tollbooth", "Norton", "Juster", 255, true);
+addBookToLibrary(testBook1);
 testBook2 = new Book("Holes", "Louis", "Sachar", 272, true);
+addBookToLibrary(testBook2);
 testBook3 = new Book("Bridge to Terabithia", "Katherine", "Paterson", 208, true);
+addBookToLibrary(testBook3);
 testBook4 = new Book("The Catcher in the Rye", "J.D.", "Salinger", 234, false);
+addBookToLibrary(testBook4);
 
-myLibrary.push(testBook1, testBook2, testBook3, testBook4);
 displayLibrary();
 showAddBookForm();
+setUpDeleteListener();
 setUpAddToLibraryListener();
